ce501c14ecafbf3d0147b761e9a9860cc43756fb
Espoir Makiese <44072742+Espoir333@users.noreply.github.com>
2020-07-24 23:31:18 -0400

    Ass1

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 'use strict'
 const x=10
 
-let videos =[
+let movies =[
     {title: "titanic", director:"James Cameron", genre: "romance", released:1997},
     {title: "mission impossible", director:"Brian De Palma", genre: "action", release:1996},
     {title: "The Seven Samurai", director:"Akira Kurosawa", genre: "action", release:1954},
@@ -12,27 +12,27 @@ let videos =[
 exports.getAll = () => {
     return movies;
 };
-
-exports.getmovies = (title) => {
-    return movies.find((movies) => {
-      return movies.title === title;
-    });
-  }
-exports.delete = (title) => {
-    const oldLength = movies.length;
-    movies = movies.filter((item) => {
-        return item.title !== title;
-    });
-    // if old & new array lengths differ, item was deleted
-    return {deleted: oldLength !== movies.length, total: movies.length };
-}; 
-exports.add = (newmovie) => {
-  const oldLength = movies.length;
-  // use existing get() method to check if movies already in our list
-  let found = this.getmovies(newmovie.title);
-  if (!found) {
-      movies.push(newmovie);
-  }
-  // if old & new array lengths differ, item was added
-  return {added: oldLength !== movies.length, total: movies.length };
-};
+console.log(this.getAll())
+// exports.getmovies = (title) => {
+//     return movies.find((movies) => {
+//       return movies.title === title;
+//     });
+//   }
+// exports.delete = (title) => {
+//     const oldLength = movies.length;
+//     movies = movies.filter((item) => {
+//         return item.title !== title;
+//     });
+//     // if old & new array lengths differ, item was deleted
+//     return {deleted: oldLength !== movies.length, total: movies.length };
+// }; 
+// exports.add = (newmovie) => {
+//   const oldLength = movies.length;
+//   // use existing get() method to check if movies already in our list
+//   let found = this.getmovies(newmovie.title);
+//   if (!found) {
+//       movies.push(newmovie);
+//   }
+//   // if old & new array lengths differ, item was added
+//   return {added: oldLength !== movies.length, total: movies.length };
+// };
