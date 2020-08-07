b490cbf2676ae300887d19f4ca659710aae6f8c0
Espoir Makiese <44072742+Espoir333@users.noreply.github.com>
2020-08-07 17:26:22 -0400

    Ass3

--- test.js
+++ test.js
@@ -1,13 +1,13 @@
 const expect = require("chai").expect;
-const movies = require("./data");
+const movies = require("../data");
 // describe("movies module", () => {
  it("returns requested movies", () => {
-   const result = movies.getmovie("titanic");
-   expect(result).to.deep.equal({title: "titanic", director:"James Cameron", genre:"romance", release:1997});
+   const result = movies.getmovies("titanic");
+   expect(result).to.deep.equal({title: "titanic", director:"James Cameron", genre:"romance", year:1997});
  });
  
  it("fails w/ invalid movies", () => {
-   const result =movies.getmovie("fake");
+   const result =movies.getmovies("fake");
    expect(result).to.be.undefined;
  });
 // });
@@ -32,5 +32,3 @@ const movies = require("./data");
     const result =movies.add("fake");
     expect(result.added).to.be.false;
   });
-
- 
