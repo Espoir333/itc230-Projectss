719acd07968bc4179210f9f0e2c9808cc1427804
Espoir Makiese <44072742+Espoir333@users.noreply.github.com>
2020-08-11 18:08:58 -0400

    Ass4

--- models/contest.js
+++ models/contest.js
@@ -1,1 +1,9 @@
+const Movie = require("../model/movies")
 
+Movie.find({}).lean()
+    .then((movies) => {
+        console.log(movies);
+    })    
+.catch(err => console.log(err));
+//Book.find({}).lean()
+ 
