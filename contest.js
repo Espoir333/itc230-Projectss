6463ab1cfcf278407faaea6c381bbf80a7b3915e
Espoir Makiese <44072742+Espoir333@users.noreply.github.com>
2020-08-11 18:06:26 -0400

    Delete contest.js

--- contest.js
+++
@@ -1,9 +0,0 @@
-const Movie = require("../model/movies")
-
-Movie.find({}).lean()
-    .then((movies) => {
-        console.log(movies);
-    })    
-.catch(err => console.log(err));
-//Book.find({}).lean()
- 
